233f4a24f31f702bcfc143c52ba4ae3e1f905bce
Nikki Meyer <nikki.meyer@codeup.com>
2023-06-26 11:19:34 -0500

    finished databases and tables lecture

---
+++ databases_tables_lecture.sql
@@ -0,0 +1,27 @@
+SHOW DATABASES;
+USE employees;
+SELECT database();
+SHOW CREATE DATABASE employees;
+
+-- CREATE TABLE somerville(
+-- 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     first_name VARCHAR(20),
+--     last_name VARCHAR(100),
+--     phone_number CHAR(10),
+--     address VARCHAR(100),
+--     ssn CHAR(9),
+--     mother_maiden VARCHAR(100),
+--     childhood_pet VARCHAR(100),
+--     childhood_street VARCHAR(100),
+--     PRIMARY KEY (id)
+-- );
+
+
+SELECT DATE('2023-04-17 13:11:09');
+SELECT TIME('2023-04-17 13:11:09');
+SELECT CURDATE();
+SELECT NOW();
+
+
+
+
