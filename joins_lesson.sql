302acfff408eb650edbf605799de6989f82784a4
Adam Krull <aqkrull@gmail.com>
2023-06-29 09:50:31 -0500

    joins lesson

---
+++ joins_lesson.sql
@@ -0,0 +1,27 @@
+#Show all databases
+SHOW DATABASES;
+
+#Select the database for the joins lesson
+USE join_example_db;
+
+#List all tables
+SHOW TABLES;
+
+#Check out what's in the roles table
+SELECT *
+FROM roles;
+
+#Check out what's in the users table
+SELECT *
+FROM users;
+
+#Inner join
+
+
+#Left join
+
+
+#Right join
+
+
+#Associative tables using employees database
