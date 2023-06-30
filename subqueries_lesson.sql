7d6e22105d55b44fc4ec88b693e42ea89ed0624c
Adam Krull <aqkrull@gmail.com>
2023-06-30 10:39:48 -0500

    add prompts

--- subqueries_lesson.sql
+++ subqueries_lesson.sql
@@ -2,12 +2,18 @@
 #Use albums_db!
 USE albums_db;
 
+#What albums had above-average sales?
+
 
 #Column subqueries
 #Use employees!
 USE employees;
 
+#Can we combine information from employees and managers without joins?
+
 
 #Table subqueries
 #Use employees!
 USE employees;
+
+#We can join to the results of a subquery!
